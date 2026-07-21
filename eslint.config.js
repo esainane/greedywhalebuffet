36a66e067202439500b7917467ad02dc76647e5d
Sai Nane <esainane+git@gmail.com>
2026-07-21 09:16:55 +0000

    fixup! Fix linting

--- eslint.config.js
+++ eslint.config.js
@@ -10,7 +10,7 @@ export default defineConfig([
 	]),
 
 	{
-		files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
+		files: ["**/*.{js,mjs,cjs,jsx,ts,mts,cts,tsx}"],
 
 		extends: [
 			js.configs.recommended,

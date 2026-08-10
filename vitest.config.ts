2582f6f807ef1eed6e8c928b9c401ff42606582b
Sai Nane <esainane+git@gmail.com>
2026-08-10 05:17:25 +0000

    Use vitest projects
    Seems a little cleaner than separate config files for now.
    Easier to review, and less risk of desync, when together.

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,44 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
 	test: {
-		environment: 'node',
+		projects: [
+			{
+				test: {
+					name: 'data-schema',
+					environment: 'node',
+					include: ['src/data.schema.test.ts'],
+				},
+			},
+			{
+				test: {
+					name: 'data',
+					environment: 'node',
+					include: ['src/**/data.*.test.ts'],
+					exclude: ['src/data.schema.test.ts']
+				},
+			},
+			{
+				test: {
+					name: 'unit',
+					environment: 'node',
+					include: ['src/**/*.test.ts'],
+					exclude: ['src/**/data.*.test.ts', 'src/e2e.schema.test.ts', 'src/e2e.schema.test.ts']
+				},
+			},
+			{
+				test: {
+					name: 'e2e-export',
+					environment: 'node',
+					include: ['src/e2e.schema.test.ts']
+				},
+			},
+			{
+				test: {
+					name: 'ui',
+					environment: 'jsdom',
+					include: ['src/**/*.test.tsx'],
+				},
+			},
+		],
 	},
 });

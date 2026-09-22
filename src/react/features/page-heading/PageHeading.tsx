5df4b1444b97d54b8342f87d3d73ebb003cd08e6
Sai Nane <esainane+git@gmail.com>
2026-09-22 20:51:35 +1200

    PageHeading: Use RichText for highlighting

--- src/react/features/page-heading/PageHeading.tsx
+++ src/react/features/page-heading/PageHeading.tsx
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { useMemo } from 'react';
+import { useCatalog } from '../../context/selectors.js';
+import { RichText } from '../../shared/richText';
+import { RichCharacterReference } from '../../../types';
 
 export function PageHeading(): React.JSX.Element {
+	const catalog = useCatalog();
+	const characters = useMemo<readonly RichCharacterReference[]>(
+		() => catalog?.textNameTeams ?? [],
+		[catalog],
+	);
+
 	return (
 		<section className="panel hero">
 			<h1>Greedy Whalebuffet</h1>
 			<p className="lede">
 				Not your normal choose your character Clocktower!
 			</p>
-			<p>You don&apos;t directly choose. You don&apos;t draft from a small set of options. You pick a set of characters you are willing to play, giving the Storyteller at least two normal Townsfolk, Outsiders, Minions, and Demons to work with, and they create a balanced and curated game from everyone&apos;s picks!</p>
-			<p />
+			<p>
+				<RichText
+					text="You don&apos;t directly choose. You don&apos;t draft from a small set of options. You pick a set of characters you are willing to play, giving the Storyteller at least two normal Townsfolk, Outsiders, Minions, and Demons to work with, and they create a balanced and curated game from everyone&apos;s picks!"
+					characters={characters}
+				/>
+			</p>
 			<a href="https://discord.gg/Px3wvyGC4w" target="_blank" rel="noopener noreferrer" className="discord-link">
 				<img src="https://cdn.simpleicons.org/discord/ffffff" alt="" aria-hidden="true" />
 				Discord guild
